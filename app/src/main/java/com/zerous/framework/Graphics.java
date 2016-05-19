57230196b5602072d8e820c455826db8f14c4dfc
Weiipad <478735862@qq.com>
2016-05-19 22:43:17 +0800

    Added

--- app/src/main/java/com/zerous/framework/Graphics.java
+++ app/src/main/java/com/zerous/framework/Graphics.java
@@ -1,38 +1,105 @@
 package com.zerous.framework;
+import android.content.res.*;
 import android.graphics.*;
+import java.io.*;
 
 public class Graphics
 {
 	Paint paint;
 	Canvas canvas;
-	
+	AssetManager asset;
+
 	public static enum PixmapFormat
 	{
 		ARGB8888, ARGB4444, RGB565
-	}
-	
-	public Graphics(Bitmap framebuffer)
+		}
+
+	public Graphics(AssetManager am, Bitmap framebuffer)
 	{
+		asset = am;
 		canvas = new Canvas(framebuffer);
 		paint = new Paint();
 	}
-	
-	public Bitmap newBitmap()
+
+	public Pixmap newPixmap(String fileName, PixmapFormat format)
 	{
+		Bitmap.Config config = null;
+		switch (format)
+		{
+			case RGB565:
+				config = Bitmap.Config.RGB_565;
+				break;
+
+			case ARGB4444:
+				config = Bitmap.Config.ARGB_4444;
+				break;
+
+			case ARGB8888:
+				config = Bitmap.Config.ARGB_8888;
+				break;
+		}
+
+		BitmapFactory.Options o = new BitmapFactory.Options();
+		o.inPreferredConfig = config;
+
+		InputStream in = null;
+		Bitmap b = null;
+		try
+		{
+			in = asset.open(fileName);
+			b = BitmapFactory.decodeStream(in);
+			if (b == null)
+			{
+				//throw xxx
+			}
+		}
+		catch (IOException e)
+		{
+			//throw xxx
+		}
+		finally
+		{
+			if (in != null)
+			{
+				try
+				{
+					in.close();
+				}
+				catch (IOException e)
+				{
+					
+				}
+			}
+		}
 		
+		switch (b.getConfig())
+		{
+			case RGB_565:
+				format = PixmapFormat.RGB565;
+				break;
+
+			case ARGB_4444:
+				format = PixmapFormat.ARGB4444;
+				break;
+
+			case ARGB_8888:
+				format = PixmapFormat.ARGB8888;
+				break;
+		}
+		return new Pixmap(b, format);
 	}
-	
+
 	public void clear(int color)
 	{
 		canvas.drawColor(color);
 	}
-	
+
 	public void drawText(String text, int x, int y)
 	{
 		paint.setAlpha(255);
 		canvas.drawText(text, x, y, paint);
 	}
-	
+
 	public void drawBitmap(Bitmap bitmap, int x, int y, int alpha)
 	{
 		paint.setAlpha(alpha);
---
+++ app/src/main/java/com/zerous/framework/Input.java
@@ -0,0 +1,15 @@
+package com.zerous.framework;
+
+public class Input
+{
+	public static class TouchEvent
+	{
+		public static final int TOUCH_DOWN = 0;
+		public static final int TOUCH_UP = 1;
+		public static final int TOUCH_MOVE = 2;
+		
+		public int type;
+		public int x, y;
+		public int pointer;
+	}
+}
