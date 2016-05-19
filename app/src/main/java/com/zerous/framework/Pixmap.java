3e1c5584058064ca08be2d5994c4f0ee1a9e4f56
Weiipad <478735862@qq.com>
2016-05-19 21:19:51 +0800

    Pixmap added

--- app/src/main/java/com/zerous/framework/Pixmap.java
+++ app/src/main/java/com/zerous/framework/Pixmap.java
@@ -1,9 +1,43 @@
 package com.zerous.framework;
 
 import android.graphics.*;
+import com.zerous.framework.Graphics.PixmapFormat;
 
 public class Pixmap
 {
 	Bitmap bitmap;
 	PixmapFormat format;
+	
+	public Pixmap(Bitmap bitmap, PixmapFormat format)
+	{
+		this.bitmap = bitmap;
+		this.format = format;
+	}
+	
+	public void scale(float xy)
+	{
+		Matrix m = new Matrix();
+		m.postScale(xy, xy);
+		bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, false);
+	}
+	
+	public int getWidth()
+	{
+		return bitmap.getWidth();
+	}
+	
+	public int getHeight()
+	{
+		return bitmap.getHeight();
+	}
+	
+	public PixmapFormat getFormat()
+	{
+		return format;
+	}
+	
+	public void dispose()
+	{
+		bitmap.recycle();
+	}
 }
